24c152678c6fea3fc964a742823f9abe9d5af810
Brian Loyal <bloyal@amazon.com>
2026-03-25 14:50:06 -0500

    Initial agent deployment framework

---
+++ infrastructure/deploy-agents.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STACK_OPERATION=$1
+
+if [[ "$STACK_OPERATION" == "create" || "$STACK_OPERATION" == "update" ]]; then
+    # deploy / update workshop resources
+    echo "Hello"
+elif [ "$STACK_OPERATION" == "delete" ]; then
+    # delete workshop resources
+    echo "Goodbye"
+else
+    echo "Invalid stack operation!"
+    exit 1
+fi
